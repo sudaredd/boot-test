905515166dc1dc2d6f6fc413159d4246f9efa342
sudarsana kasireddy <sudaredd@gmail.com>
2022-10-16 20:46:06 -0400

    update sql

--- src/test/resources/scripts/INIT_THREE_ORDERS.sql
+++ src/test/resources/scripts/INIT_THREE_ORDERS.sql
@@ -1,6 +1,3 @@
-INSERT INTO orders (tracking_number, items)
-VALUES ('42', '[{"name": "MacBook Pro", "amount" : 42}, {"name": "iPhone Pro", "amount" : 42}]');
-INSERT INTO orders (tracking_number, items)
-VALUES ('43', '[{"name": "Kindle", "amount" : 13}, {"name": "MacBook Pro", "amount" : 10}]');
-INSERT INTO orders (tracking_number, items)
-VALUES ('44', '[]');
+INSERT INTO orders (tracking_number, items) VALUES ('42', '[{"name": "MacBook Pro", "amount" : 42}, {"name": "iPhone Pro", "amount" : 42}]');
+INSERT INTO orders (tracking_number, items) VALUES ('43', '[{"name": "Kindle", "amount" : 13}, {"name": "MacBook Pro", "amount" : 10}]');
+INSERT INTO orders (tracking_number, items) VALUES ('44', '[]');
